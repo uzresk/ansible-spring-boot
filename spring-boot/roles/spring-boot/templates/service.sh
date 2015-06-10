581797d09e188fdbd4aa1c0e440bb39a3ac37a16
uzresk <uzr.esk@gmail.com>
2015-06-10 09:24:28 +0000

    spring-boot modify service.sh template

--- spring-boot/roles/spring-boot/templates/service.sh
+++ spring-boot/roles/spring-boot/templates/service.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 
 JAVA_HOME=/usr/java/default
-APP_NAME: {{APP_NAME}}
-BASE_DIR: {{APP_NAME}}
-JAR_FILE: {{JAR_FILE}}
-LOG_CONFIG_FILE: {{LOG_CONFIG_FILE}}
-PID: {{PID}}
-PROFILE: {{PROFILE}}
+APP_NAME={{APP_NAME}}
+BASE_DIR={{BASE_DIR}}
+JAR_FILE={{JAR_FILE}}
+LOG_CONFIG_FILE={{LOG_CONFIG_FILE}}
+PID={{PID}}
+PROFILE={{PROFILE}}
 
 case "$1" in
   "start" )
-    if [ ! -f $PID ]; then
-      echo "Starting $APP_NAME"
-      $JAVA_HOME/bin/java -Xbootclasspath/a:. -Dspring.profiles.active=$PROFILE -Dlogging.config=$LOG_CONFIG_FILE -jar $JAR_FILE &
-      echo $! > $PID
+    if [ ! -f ${PID} ]; then
+      echo "Starting ${APP_NAME}"
+      ${JAVA_HOME}/bin/java -Xbootclasspath/a:. -Dspring.profiles.active=${PROFILE} -Dlogging.config=${LOG_CONFIG_FILE} -jar ${JAR_FILE} &
+      echo $! > ${PID}
     else
-      echo "$APP_NAME is Running."
+      echo "${APP_NAME} is Running."
     fi
     ;;
   "stop" )
-    echo "Stopping $APP_NAME"
-    kill `cat $PID`
+    echo "Stopping ${APP_NAME}"
+    kill `cat ${PID}`
     RETVAL=$?
     if test $RETVAL -eq 0 ; then
-        rm -f $PID
+        rm -f ${PID}
     fi
     ;;
   *)
