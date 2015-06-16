cd6a56d80bda9d7dc667bbaf15b9e2e548eddf7c
uzresk <uzr.esk@gmail.com>
2015-06-16 07:13:57 +0000

    delete user_spec.rb, spring-boot-spec.rb add user settings

--- serverspec/spec/192.168.1.20/spring-boot_spec.rb
+++ serverspec/spec/192.168.1.20/spring-boot_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
-describe file('/home/cmp/cmp/service.sh') do
+describe user('cmp') do
+  it { should exist }
+  it { should belong_to_group 'cmp' }
+  it { should have_uid 501 }
+end
+
+describe file('/home/cmp/app/service.sh') do
   it { should be_file }
 end
 
-describe file('/home/cmp/cmp/logback.xml') do
+describe file('/home/cmp/app/logback.xml') do
   it { should be_file }
 end
